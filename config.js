25cde15a3cf61f6d176811b9027e7f9735f8e4a7
skylego <101873337+skylego@users.noreply.github.com>
2023-06-07 22:47:31 +0800

    Update config.js

--- config.js
+++ config.js
@@ -3,47 +3,52 @@ module.exports = {
     {
       'key': 'google',
       'proxied': 'https://www.google.com/',
-      'proxying': 'google.upupming.site'
+      'proxying': 'google.site'
     },
     {
       'key': 'scholar.google',
       'proxied': 'https://scholar.google.com/',
-      'proxying': 'scholar.google.upupming.site'
+      'proxying': 'scholar.google.site'
     },
     {
       'key': 'maps.google',
       'proxied': 'https://maps.google.com/',
-      'proxying': 'maps.google.upupming.site'
+      'proxying': 'maps.google.site'
     },
     {
       'key': 'translate.google',
       'proxied': 'https://translate.google.com/',
-      'proxying': 'translate.google.upupming.site'
+      'proxying': 'translate.google.site'
     },
     {
       'key': 'docs.google',
       'proxied': 'https://docs.google.com/',
-      'proxying': 'docs.google.upupming.site'
+      'proxying': 'docs.google.site'
     },
     {
       'key': 'drive.google',
       'proxied': 'https://drive.google.com/',
-      'proxying': 'drive.google.upupming.site'
+      'proxying': 'drive.google.site'
     },
     {
       'key': 'code.google',
       'proxied': 'https://code.google.com/',
-      'proxying': 'code.google.upupming.site'
+      'proxying': 'code.google.site'
+    },
+    {
+      'key': 'youtube',
+      'proxied': 'https://www.youtube.com/',
+      'proxying': 'youtube.site'
     },
     {
       'key': 'wiki',
       'proxied': 'https://zh.wikipedia.org/',
-      'proxying': 'wiki.upupming.site'
+      'proxying': 'wiki.site'
     },
     {
       'key': 'mwiki',
       'proxied': 'https://zh.m.wikipedia.org/',
-      'proxying': 'mwiki.upupming.site'
+      'proxying': 'mwiki.site'
     }
   ]
 }
